775cd3b2db101742038a5fe5bb7ee98b7a88cb55
Daniel Fejdych <df2017@hw.ac.uk>
2023-09-29 13:06:41 +0100

    Finished a working draft of the simple paint program.
    Finished a working draft of the simple paint program allowing it to paint pixels.

--- Simple photo editor/script.js
+++ Simple photo editor/script.js
@@ -1,6 +1,5 @@
 var curColor;
 
-
 function getColor() {
 var redVal = document.getElementById("redB").value;
 var greenVal = document.getElementById("greenB").value;
@@ -11,4 +10,30 @@ return "rgb(" + redVal + ", " + greenVal + ", " + blueVal  + ")";
 function setColor() {
 	curColor = getColor();
 }
+function color(eID){
+	document.getElementById(eID).style.backgroundColor = getColor();
+}
+
+
+function createTable() {
+	let tab = "<table>";
+	console.log(tab);
+	for (let i = 0; i < 100; i++) {
+		tab += "<tr>";
+		for (let j = 0; j < 100; j++) {
+			tab += "<td onclick = \"color('t" + i + j + "')\" id = \"t" + i + j + "\">&nbsp;</td>";
+		}
+		tab += "</tr>";
+	}
+	tab += "</table>";
+	console.log(tab);
+	return tab;
+}
+
+
+
+
+
+document.getElementById("tabHere").innerHTML += createTable();
+
 //document.appendChild();
