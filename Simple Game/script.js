0d1876cf21f7404712e051e5d81c6879f4d0134d
Daniel Fejdych <df2017@hw.ac.uk>
2023-10-12 10:15:25 +0100

    Update script.js
    Will attempt to find a way to push the buttons. TBD later.

--- Simple Game/script.js
+++ Simple Game/script.js
@@ -3,5 +3,7 @@ $(document).ready(function(){
       var options = ['Click This', 'Not this', 'Maybe this one.'];
       options.forEach(function(option){
       var button = $('<button>').text(option);
+      button.cssText = "position: absolute; top: 100px; right: 10px; width: 100px; height: 80px;";
       button.appendTo('#btn');
+})
 })
