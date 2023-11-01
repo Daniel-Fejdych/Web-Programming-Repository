6c59c97d390a06b22ccf8f40df55b07eecbc8de2
Daniel Fejdych <df2017@hw.ac.uk>
2023-11-01 19:34:46 +0000

    Added the first rss feed
    Added the first rss feed with all the current news.

---
+++ RSS Feed Example/script.js
@@ -0,0 +1,49 @@
+function resolveAfter2Seconds() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve('resolved');
+    }, 2000);
+  });
+}
+
+async function asyncCall() {
+
+
+let prom = await fetch( 'https://moxie.foxnews.com/google-publisher/latest.xml' );
+let text = await prom.text();
+
+console.log( text.substring(0, 100 ) );
+
+let parser = new DOMParser();
+let xmlDoc = parser.parseFromString(text, "text/xml");
+
+console.log( xmlDoc );
+
+const items = xmlDoc.querySelectorAll("item");
+console.log('items:', items );
+
+items.forEach( (el)=>{
+    let title       = el.querySelector('title').textContent;
+    let description = el.querySelector('description').textContent;
+    let guid        = el.querySelector('guid').textContent;
+
+    console.log('title:',       title       );
+    console.log('description:', description);
+    console.log('guid:',        guid);
+
+    let div = document.createElement('div');
+    document.body.appendChild( div );
+    div.innerHTML = `
+        <h2>${title}</h2>
+        <p>${description}</p>
+    `;
+    if ( guid ) div.innerHTML += `<a href='${guid}'>LINK</a>`;
+
+});
+
+}
+$(function() {
+
+asyncCall();
+})
+
